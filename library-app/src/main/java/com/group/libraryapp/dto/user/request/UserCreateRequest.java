5105a19e81436f5efbff23566e6f9774a780c521
minjun <hitobi1014@gmail.com>
2024-11-06 12:51:19 +0900

    test. user 테스트코드 작성, dto 생성자 추가

--- library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -1,16 +1,27 @@
 package com.group.libraryapp.dto.user.request;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class UserCreateRequest {
 
-  private String name;
-  private Integer age;
+    @NotNull
+    private String name;
+    @Nullable
+    private Integer age;
 
-  public String getName() {
-    return name;
-  }
+    public UserCreateRequest(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  public Integer getAge() {
-    return age;
-  }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
 
 }
