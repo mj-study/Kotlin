b074b9d697f8d906abd1fdf59eadc6919b5edb4a
minjun <hitobi1014@gmail.com>
2024-11-07 13:27:50 +0900

    migration. dto Java -> Kotlin

--- library-app/src/main/kotlin/com/group/libraryapp/dto/user/request/UserCreateRequest.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/dto/user/request/UserCreateRequest.kt
@@ -1,27 +1,6 @@
-package com.group.libraryapp.dto.user.request;
+package com.group.libraryapp.dto.user.request
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-public class UserCreateRequest {
-
-    @NotNull
-    private String name;
-    @Nullable
-    private Integer age;
-
-    public UserCreateRequest(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-}
+data class UserCreateRequest(
+    val name: String,
+    val age: Int?,
+)
